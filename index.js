48bf6a8ba33885a683322a8615749bf4ec8ed5a6
Hf1b <itzdaniel1337@gmail.com>
2021-05-05 14:53:12 +0500

    большая кастомизация запроса

--- index.js
+++ index.js
@@ -27,19 +27,72 @@ const prompt = message => {
 ;(async()=> {
   console.log('База:', base)
   console.log('Для выхода нажмите Ctrl+C или напишите exit')
+  console.log('Структура: method GET/POST/PUT/DELETE|endpoint URL|show FLATTEN_ADDRESS|body JSON')
   while(true) {
-    let query = (await prompt('Запрос: ')).split(' ')
-    if(query.length == 0 || !query[0]) continue
-    if(query[0] == 'exit') process.exit()
+    let query = (await prompt('Запрос: '))
+    if(query == 'exit') process.exit()
 
-    if(!query[0].startsWith('/')) query[0] = '/' + query[0]
+    query = query.split(/\s*\|\s*/)
+    if(query.length == 0) continue
 
-    let request = await fetch(base + query[0], { headers })
+    let method = 'GET'
+    let endpoint
+    let objAddress
+    let body
+
+    for(let info of query) {
+      info = info.split(' ')
+      switch(info[0]) {
+        case 'method':
+          method = info[1]
+          break
+        case 'endpoint':
+          if(!info[1].includes(base)) {
+            if(!info[1].startsWith('/')) info[1] = '/' + info[1]
+            info[1] = base + info[1]
+          }
+          endpoint = info[1]
+          break
+        case 'show':
+          objAddress = info[1]
+          break
+        case 'body':
+          try {
+            body = JSON.parse(info[1])
+          } catch(e) {
+            console.error('Не удалось распарсить body.', e.toString())
+            continue
+          }
+          break
+        default:
+          console.log('Неизвестный параметр', info[0])
+      }
+    }
+
+    if(!endpoint) {
+      console.log('Укажите эндпоинт в следующий раз.')
+      continue
+    }
+
+    console.log('Метод:', method)
+    console.log('Эндпоинт:', endpoint)
+    console.log('Показать объект:', objAddress ? objAddress : 'весь')
+    console.log('Тело запроса:', body)
+    let sure = await prompt('Вы уверены (y/да/n/нет)? ')
+    if(!['y', 'да'].includes(sure)) {
+      console.log('Запрос отменён.')
+      continue
+    }
+
+    let request = await fetch(endpoint, { body, headers, method })
     let output = await request.json()
-    if(query.length > 1) {
-      let keys = query[1].split('.')
+    if(objAddress) {
+      let keys = objAddress.split('.')
       for(let key of keys) {
-        if(!output[key]) break
+        if(!output[key]) {
+          console.log('Предупреждение: отсутсвует ключ', key, 'указанный в objAddress')
+          break
+        }
         output = output[key]
       }
     }
